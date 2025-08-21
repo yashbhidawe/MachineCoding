a410a5744c8b3608a8ed925b0af2a45d03a90142
Yash Bhidawe <ybh5179@gmail.com>
2025-08-21 14:00:20 +0530

    add: capitlize

---
+++ src/JS/capitalize.js
@@ -0,0 +1,9 @@
+function capitalizeWords(sentence) {
+  return sentence
+    .trim()
+    .split(/\s+/)
+    .map((word) => word.charAt(0).toUpperCase() + word.slice(1).toLowerCase())
+    .join(" ");
+}
+
+export default capitalizeWords;
