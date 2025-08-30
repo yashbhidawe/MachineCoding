fe608ed2748accafecf292144a080563a3b870a4
Yash Bhidawe <ybh5179@gmail.com>
2025-08-30 14:00:00 +0530

    add: power of four

---
+++ src/JS/isPowerOfFour.js
@@ -0,0 +1,8 @@
+function isPowerOfFour(n) {
+  // your function implementation
+  if (n <= 0) return false;
+  if (n === 1) return true;
+  if (n % 4 !== 0) return false;
+
+  return isPowerOfFour(n / 4);
+}
